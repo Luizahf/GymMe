cb8ba750b77489ebac159426cd75128d3aba4185
Luiza Freire <luiza.freire@localiza.com>
2020-03-26 15:25:23 -0300

    Initial layout and go to my worksheet implementation

--- APP/mobile/src/main/java/com/raywenderlich/gymme/SplashScreen/SplashScreenActivity.kt
+++ APP/mobile/src/main/java/com/raywenderlich/gymme/SplashScreen/SplashScreenActivity.kt
@@ -1,21 +1,21 @@
 package com.raywenderlich.gymme.SplashScreen
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import com.raywenderlich.gymme.MyWorksheet.MyWorksheetActivity
 import com.raywenderlich.gymme.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity(), View.OnClickListener {
-
-    private lateinit var btnGoToWorksheet: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        btnGoToWorksheet = findViewById(R.id.btn_worksheet)
-        btnGoToWorksheet.setOnClickListener(this)
+        btn_worksheet.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
@@ -23,12 +23,12 @@ class SplashScreenActivity : AppCompatActivity(), View.OnClickListener {
 
         when (id) {
             R.id.btn_worksheet -> {
-                goToWorksheet()
+                goToWorksheetExercises()
             }
         }
     }
 
-    fun goToWorksheet() {
-        //val intent = Intent(this, )
+    fun goToWorksheetExercises() {
+        startActivity(Intent(this, MyWorksheetActivity::class.java))
     }
 }
